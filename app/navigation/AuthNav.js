5c987d30dc968b6fe44af988e2a93581e5c266bc
team-cake <eka.cheuk@gmail.com>
2020-09-14 17:04:43 +0200

    Created AppNav & AuthNav and implement the different screens

---
+++ app/navigation/AuthNav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import LogInScreen from '../screens/LogInScreen'
+import SignUpScreen from '../screens/SignUpScreen'
+import SignUpConfScreen from '../screens/SignUpConfScreen'
+
+const Stack = createStackNavigator()
+
+export default function AuthStack() {
+	return (
+		<Stack.Navigator screenOptions={{ headerShown: false }}>
+			<Stack.Screen name='Login' component={LogInScreen} />
+			<Stack.Screen name='SignUp' component={SignUpScreen} />
+			<Stack.Screen name='SignUpConf' component={SignUpConfScreen} />
+		</Stack.Navigator>
+	)
+}
