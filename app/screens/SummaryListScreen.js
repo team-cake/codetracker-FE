6da1e9bc8020cd64b57c08c4918d08f772cabf2a
team-cake <eka.cheuk@gmail.com>
2020-09-15 18:19:39 +0200

    Summaries available on SummaryListScreen.js

--- app/screens/SummaryListScreen.js
+++ app/screens/SummaryListScreen.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
+import { getSummaries } from '../store/summary/selector'
+import { fetchSummaries } from '../store/summary/actions'
 
 export default function SummaryListScreen() {
+	const dispatch = useDispatch()
+	const summaries = useSelector(getSummaries)
+	console.log('SummaryListScreen -> summaries', summaries.summaries)
+
+	useEffect(() => {
+		dispatch(fetchSummaries())
+	}, [dispatch])
+
 	return (
 		<View style={styles.container}>
-			<Text>SummaryListScreen</Text>
+			<Text>SummaryListScreen, what what</Text>
+			{/* {summaries.map((s) => {
+				return <Text>{s.description}</Text>
+			})} */}
 		</View>
 	)
 }
