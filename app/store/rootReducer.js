a0d79227139c7a66bc25e53f174a938568d40035
team-cake <eka.cheuk@gmail.com>
2020-09-15 18:25:24 +0200

    Adjusted rootReducer, added summary reducer

--- app/store/rootReducer.js
+++ app/store/rootReducer.js
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
 import { combineReducers } from 'redux'
 import appState from './appState/reducer'
+import topics from './topics/reducer'
+import topicDetails from './topicDetails/reducer'
 import summary from './summary/reducer'
+import user from './user/reducer'
 
 export default combineReducers({
 	appState,
+	user,
 	topics,
 	topicDetails,
+	summary,
 })
-=======
-import { combineReducers } from "redux";
-import appState from "./appState/reducer";
-import topics from "./topics/reducer";
-import topicDetails from "./topicDetails/reducer";
-export default combineReducers({
-  appState,
-  topics,
-  topicDetails,
-});
->>>>>>> 1e2bd9650b09fd2f9e95456974e0a83d6ddb7339
