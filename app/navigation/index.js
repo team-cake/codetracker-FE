186a7fda16d1300f45220088d667eeb39b7df796
tom <tomvosmusic@gmail.com>
2020-09-16 10:07:07 +0200

    updated root navigator now that logging in is possible

--- app/navigation/index.js
+++ app/navigation/index.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { useSelector } from 'react-redux'
+import { selectUser } from '../store/user/selectors'
 import AuthNav from './AuthNav'
 import AppNav from './AppNav'
 
@@ -16,19 +17,20 @@ export default function Navigation() {
 const Stack = createStackNavigator()
 
 function RootNavigator() {
-	const user = useSelector((state) => state.user)
+	const user = useSelector(selectUser)
+	console.log({user})
 	return (
 		<Stack.Navigator screenOptions={{ headerShown: false }}>
-			{/* {(!user || !user.token) && (
+			{(!user || !user.token) && (
 				<Stack.Screen name='AuthNav' component={AuthNav} />
 			)}
-			{user && user.token && <Stack.Screen name='AppNav' component={AppNav} />} */}
+			{user && user.token && <Stack.Screen name='AppNav' component={AppNav} />}
 			{/* When the user is able to log in, we will use above logic */}
 
-			<Stack.Screen name='AuthNav' component={AuthNav} />
+			{/* <Stack.Screen name='AuthNav' component={AuthNav} /> */}
 			{/* This one is for the login and sign up */}
 
-			<Stack.Screen name='AppNav' component={AppNav} />
+			{/* <Stack.Screen name='AppNav' component={AppNav} /> */}
 			{/* this one is for the dashboard etc, comment out line 28 and refresh if you want to see dashboard etc */}
 		</Stack.Navigator>
 	)
