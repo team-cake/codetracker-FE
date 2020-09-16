8ef28bd35ac607c8e8ccd2161ade22283785949d
team-cake <eka.cheuk@gmail.com>
2020-09-16 11:35:27 +0200

    Added code so summaries of logged in user is displayed

--- app/screens/SummaryListScreen.js
+++ app/screens/SummaryListScreen.js
@@ -1,13 +1,21 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import { fetchSummaries } from '../store/summary/actions'
-import { selectSummaries } from '../store/summary/selector'
+import { selectSummaries } from '../store/summary/selectors'
+import { selectUserSummaries } from '../store/user/selectors'
+import { selectUser } from '../store/user/selectors'
+import { Button } from 'monalisa-ui'
 
 export default function SummaryListScreen({ navigation }) {
 	const dispatch = useDispatch()
 	const { summaries } = useSelector(selectSummaries)
 	console.log('SummaryListScreen -> summaries', summaries)
+
+	const summary = useSelector(selectUserSummaries)
+	console.log('SummaryListScreen -> summary', summary)
+
+	const user = useSelector(selectUser)
 
 	useEffect(() => {
 		dispatch(fetchSummaries())
@@ -15,9 +23,30 @@ export default function SummaryListScreen({ navigation }) {
 
 	return (
 		<View style={styles.container}>
-			<Text style={styles.small}>SummaryListScreen - her TEAM PREMIER</Text>
+			<Text style={styles.small}>SummaryListScreen - hey TEAM PREMIER</Text>
+			<View style={{ height: 20 }} />
+			<Text>Below is all summaries</Text>
 			{summaries ? (
 				summaries.map((s) => {
+					return (
+						<Button
+							key={s.id}
+							title={s.description}
+							onPress={() =>
+								navigation.navigate('TopicDetail', {
+									id: s.id,
+								})
+							}
+						/>
+					)
+				})
+			) : (
+				<Text>Loading...</Text>
+			)}
+			<View style={{ height: 20 }} />
+			<Text>Here below is {user.name}'s summary</Text>
+			{summary ? (
+				summary.map((s) => {
 					return (
 						<Button
 							key={s.id}
