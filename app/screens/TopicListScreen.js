a9cc2fc98f668705e760b668e8000015d190eac1
Odett3 <67112041+Odett3@users.noreply.github.com>
2020-09-16 11:29:00 +0200

    added a picker selector for all the weeks in the academy, to do this I installed a library called react-native-picker-select

--- app/screens/TopicListScreen.js
+++ app/screens/TopicListScreen.js
@@ -1,43 +1,69 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { StyleSheet, Button, View } from 'react-native'
-import { fetchTopics } from '../store/topics/actions'
-import { selectTopics } from '../store/topics/selectors'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { StyleSheet, Button, View, PickerIOSBase } from "react-native";
+import { fetchTopics } from "../store/topics/actions";
+import { selectTopics } from "../store/topics/selectors";
 // import { Button } from "monalisa-ui";
+import RNPickerSelect from "react-native-picker-select";
 
 export default function TopicListScreen({ navigation }) {
-	const dispatch = useDispatch()
-	const { topics } = useSelector(selectTopics)
-	console.log('TopicListScreen -> topics', topics)
+  const dispatch = useDispatch();
+  const { topics } = useSelector(selectTopics);
+  console.log("TopicListScreen -> topics", topics);
 
-	useEffect(() => {
-		dispatch(fetchTopics())
-	}, [dispatch])
+  useEffect(() => {
+    dispatch(fetchTopics());
+  }, [dispatch]);
 
-	return (
-		<View style={styles.container}>
-			{topics.map((t) => {
-				return (
-					<Button
-						key={t.id}
-						title={t.name}
-						onPress={() =>
-							navigation.navigate('TopicDetail', {
-								id: t.id,
-							})
-						}
-					/>
-				)
-			})}
-		</View>
-	)
+  const Dropdown = () => {
+    return (
+      <RNPickerSelect
+        onValueChange={(value) => console.log(value)}
+        items={[
+          { label: "Week 1", value: "1" },
+          { label: "Week 2", value: "2" },
+          { label: "Week 3", value: "3" },
+          { label: "Week 4", value: "4" },
+          { label: "Week 5", value: "5" },
+          { label: "Week 6", value: "6" },
+          { label: "Week 7", value: "7" },
+          { label: "Week 8", value: "8" },
+          { label: "Week 9", value: "9" },
+          { label: "Week 10", value: "10" },
+          { label: "Week 11", value: "11" },
+        ]}
+      />
+    );
+  };
+  return (
+    <View style={styles.container}>
+      {Dropdown()}
+      {topics.map((t) => {
+        console.log("TopicListScreen -> t", t);
+
+        return (
+          <View>
+            <Button
+              key={t.id}
+              title={t.name}
+              onPress={() =>
+                navigation.navigate("TopicDetail", {
+                  id: t.id,
+                })
+              }
+            />
+          </View>
+        );
+      })}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
